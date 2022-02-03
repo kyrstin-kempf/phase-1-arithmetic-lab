a126c481c2d764bc3c936883cac9a2a225d300bb
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-02-02 22:39:48 -0500

    commit tree fa95be8d4c70d136296b65527a901ced07bcbc83

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+// Add 1 to result number > 0
+let random = Number.isInteger(random) + 1;
+
+// Remainder - remaining value not fully divisable
+let num3 = 84
+let num4 = 5
+let mod = num3 % num4;
+
+let max = Math.max(2, 7, 19, 3, 20);
